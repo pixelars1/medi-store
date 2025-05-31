f2b257d6b499be66b95e485b104c7a8a1c91accd
anuj407 <anuj97953a@gmail.com>
2025-05-31 19:28:17 +0530

    feat: implement routing with Home component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
 function App() {
   return (
     <div>
-      <h1 className="text-red-500">Hii</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </div>
   )
 }
