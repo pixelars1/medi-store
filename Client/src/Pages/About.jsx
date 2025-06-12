c6c150b8795ce07aafa274f8920863a2927f10c5
arshad033 <arshadobra85@gmail.com>
2025-06-12 13:03:33 +0530

    fix: update import path for Testimonials component in About page to check deployment issue

--- Client/src/Pages/About.jsx
+++ Client/src/Pages/About.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
-import Testimonials from "../Components/Testimonials";
+import Testimonials from "../Components/Testimonials.jsx";
 
 const whyUsData = [
   {
