17ba7912a5faeec8854da743f07984c7fa62345b
anuj407 <anuj97953a@gmail.com>
2025-07-27 12:34:58 +0530

    feat: add cartCount useState

--- Client/src/Context/AppContext.jsx
+++ Client/src/Context/AppContext.jsx
@@ -7,10 +7,13 @@ export const AppProvider = ({ children }) => {
    const [darkMode, setDarkMode] = useState(() => {
     return localStorage.getItem("theme") === "dark";
   });
+   const [cartCount, setCartCount] = useState(0);
   return (
     <AppContext.Provider value={{
       darkMode,
       setDarkMode,
+      cartCount,
+      setCartCount
     }}>
       {children}
     </AppContext.Provider>
