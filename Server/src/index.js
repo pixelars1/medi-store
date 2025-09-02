0d66bc1be3e23517892f16fe5f6306e1183c391c
anuj407 <anuj97953a@gmail.com>
2025-09-02 17:38:31 +0530

    fix: update entry point and Vercel configuration to use index.js

---
+++ Server/src/index.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import { connectDB } from './db/db.js';
+import app from './app.js';
+dotenv.config();
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+app.listen(process.env.PORT || 3000 , () => console.log(`server is listening on Port : ${process.env.PORT || 3000}`));
