314b892426a23808c7c6f0ac2d191c452da45348
anuj407 <anuj97953a@gmail.com>
2025-09-02 15:19:08 +0530

    fix: update Vite configuration to include path aliasing

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import path from 'path'
+
 export default defineConfig({
   plugins: [
-    react(), 
-    tailwindcss()],
+    react(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
