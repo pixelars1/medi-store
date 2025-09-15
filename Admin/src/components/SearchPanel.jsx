8dbeae8f4859edeeebfc7e08bc7780097ab26fd7
anuj407 <anuj97953a@gmail.com>
2025-09-15 22:04:50 +0530

    add to getting search details

---
+++ Admin/src/components/SearchPanel.jsx
@@ -0,0 +1,32 @@
+import { mockSearches } from "../utils/constant";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
+
+const SearchesPanel = ({ searches }) => (
+  <div className="grid gap-4">
+    <Card className="rounded-2xl shadow-sm">
+      <CardHeader>
+        <CardTitle>Top Search Terms</CardTitle>
+      </CardHeader>
+      <CardContent className="p-0">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Term</TableHead>
+              <TableHead className="text-right">Count</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {mockSearches.map((s) => (
+              <TableRow key={s.term}>
+                <TableCell className="font-medium">{s.term}</TableCell>
+                <TableCell className="text-right">{s.count}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  </div>
+);
+export default SearchesPanel;
